d73bf7e39a189c458d5ff32990d5ed37aba36a78
Kimi <cyrekamil@gmail.com>
2019-10-03 15:04:52 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var submitButton = document.getElementById("submitWeight");
+
+submitButton.onclick = function(){
+    var sumbitValue = document.getElementById("submitValue").value;
+    console.log(sumbitValue);
+    var tabValue = [];
+    tabValue.push(sumbitValue);
+    console.log(sumbitValue);
+
+} 
+
+
