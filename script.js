9a3f856566e0d0a6659dacfda749192a19852e8b
Kimi <cyrekamil@gmail.com>
2019-10-04 15:36:46 +0200

    Almost finish CSS

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 var submitButton = document.getElementById("submitWeight");
 
+
 submitButton.onclick = function(){
     var sumbitValue = document.getElementById("submitValue").value;
-    console.log(sumbitValue);
+    console.log(sumbitValue); //console log na to co wpisane w inputa
     var tabValue = [];
-    tabValue.push(sumbitValue);
-    console.log(sumbitValue);
+    
+    tabValue = sumbitValue.split(" ")
+    console.log(tabValue);
+    const input = document.getElementById("box__input");  //deleting input when it isn't need.
+    input.parentNode.removeChild(input);
+    const describe = document.getElementById("description");
+    describe.innerText = "Now enter your marks in right fields bellow";
 
+    var howManyFields = tabValue.length;
+    
+    createField(howManyFields);
 } 
 
 
+function createField(howManyFields){
+    console.log(howManyFields)
+
+}
